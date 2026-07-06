cfb1c511c12ff67657d909eb8d60eeca65e2ab02
Daniel <88160656+danieljustus@users.noreply.github.com>
2026-07-06 16:11:15 +0200

    chore: bump symaira-appkit to v0.1.2 (#58)

--- client/Package.swift
+++ client/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "SymscopeFeature", targets: ["SymscopeFeature"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/danieljustus/symaira-appkit.git", exact: "0.1.1"),
+        .package(url: "https://github.com/danieljustus/symaira-appkit.git", exact: "0.1.2"),
     ],
     targets: [
         .target(
